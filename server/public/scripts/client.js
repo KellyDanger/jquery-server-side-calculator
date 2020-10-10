6905557a9aceb83c8697d6062387f9e3c8d10c71
Kelly Danger <kellyanndanger@gmail.com>
2020-10-10 17:24:54 -0500

    operator buttons working and setting operatorChoice

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,13 +6,32 @@ function onReady(){
   getMathRecord();
   //event listeners
   $('#plusButton').on('click', operatorSetAdd);
+  $('#subtractButton').on('click', operatorSetSubtract);
+  $('#multiplyButton').on('click', operatorSetMultiply);
+  $('#divideButton').on('click', operatorSetDivide);
   
 }//end onReady
 let operatorChoice; 
 
 function operatorSetAdd (){ 
   operatorChoice = 'add'
+  console.log("we're going to", operatorChoice)
 }//end operatorSetAdd
+
+function operatorSetSubtract (){ 
+  operatorChoice = 'subtract'
+  console.log("we're going to", operatorChoice)
+}//end operatorSetSubtract
+
+function operatorSetMultiply (){ 
+  operatorChoice = 'multiply'
+  console.log("we're going to", operatorChoice)
+}//end operatorSetMultiply
+
+function operatorSetDivide (){ 
+  operatorChoice = 'divide'
+  console.log("we're going to", operatorChoice)
+}//end operatorSetDivide
 
 function getMathRecord() {
   $.ajax({
@@ -23,4 +42,4 @@ function getMathRecord() {
   }).catch(function(error){
     alert(error);
   });
-}
+}//end get mathRecord request
