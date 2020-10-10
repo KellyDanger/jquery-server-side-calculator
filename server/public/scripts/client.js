68de33043a77fbd683cab5a5f112658981cfae77
Kelly Danger <kellyanndanger@gmail.com>
2020-10-10 17:09:52 -0500

    server working get request working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,24 @@ $(document).ready(onReady);
 
 function onReady(){
   console.log('Hello from jq');
+  getMathRecord();
+  //event listeners
+  $('#plusButton').on('click', operatorSetAdd);
   
 }//end onReady
+let operatorChoice; 
+
+function operatorSetAdd (){ 
+  operatorChoice = 'add'
+}//end operatorSetAdd
+
+function getMathRecord() {
+  $.ajax({
+    method: 'GET',
+    url: '/calculations'
+  }).then(function(response){
+    console.log(response);
+  }).catch(function(error){
+    alert(error);
+  });
+}
--- server/server.js
+++ server/server.js
@@ -4,7 +4,22 @@ const bodyParser = require ('body-parser');
 
 const app = express();
 const port = 5000;
+
+app.use(express.static('server/public'));
 //end server setup
+
+//Variables
+let calculationHistory = [{
+  numInput1: 2,
+  numInput2: 3,
+  operatorChoice: 'add'
+},
+];
+
+//GET requests
+app.get('/calculations', (req, res) => {
+  res.send(calculationHistory);
+})
 
 
 app.listen(port, () => {
